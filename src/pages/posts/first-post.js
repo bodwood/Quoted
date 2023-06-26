038474c0dc52d79621027c4b9aa1243b07be718d
bodie wood <bodie1@macbook-air.lan>
2023-06-26 11:06:20 -0700

    Add page that links home page to first-post page

---
+++ src/pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const FirstPost = () => {
+  return( 
+  <div>
+   first-post
+   Click here to go back <Link href="/">home</Link>
+  </div>)
+};
+export default FirstPost;
